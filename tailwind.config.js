4bd296682c7b1f4ea6ff635c581c9c23f69ea30e
Hakizimana Clement <h.clementoert@gmail.com>
2023-09-01 20:23:10 +0200

    complete navbar for large screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter,sans-serif"],
+      },
+      colors: {
+        D_gray: {
+          l_grey: "#89939E",
+          DEFAUT: "#717171",
+          grey_blue: "#ABBED1",
+          sliver: "#F5F7FA",
+        },
+      },
+    },
   },
   plugins: [],
 };
